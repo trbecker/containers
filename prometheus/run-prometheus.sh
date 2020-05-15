b794380eecffd37f368ae3dda21533fd45a57b05
Thiago Becker <tbecker@redhat.com>
2020-05-15 11:32:05 -0300

    Common makefile

--- prometheus/run-prometheus.sh
+++ prometheus/run-prometheus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /p/prometheus --config.file=/etc/prometheus/prometheus.yml \
-              --storage.tsdb.path=/prometheus \
+              --storage.tsdb.path=/var/lib/prometheus \
               --web.console.libraries=/p/console_libraries \
               --web.console.templates=/p/consoles \
               --web.listen-address=:9091
